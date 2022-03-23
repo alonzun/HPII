02b42705791340139a78190b269746bcc7abd3b3
alonzun <alonesdeveloper@gmail.com>
2022-03-23 11:10:27 -0500

    Sesion8-Parte1

---
+++ JavaScript/js/libreria.js
@@ -0,0 +1,10 @@
+//alert("Se ve el mensaje")
+//document.write("Se ve el mensaje")
+
+var nombre='Nilo', 
+numero1=10, numero2=2,
+resultado=numero1+numero2;
+
+console.log(nombre);
+document.write(nombre)
+
---
+++ JavaScript/js/supermercado.js
@@ -0,0 +1,10 @@
+function compra(b, f, a){
+    var pago=b+f+a;
+    return pago;
+}
+
+document.write('Las compras de un dia de supermercado');
+document.write('<br>');
+document.write(compra(80,15,60));
+
+
